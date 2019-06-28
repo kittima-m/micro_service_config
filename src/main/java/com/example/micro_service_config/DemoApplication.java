57f1db9737e312b7a3cce50f9b24945be259e7be
Kittima Meesrisong <kittima.meesrisong@hotmail.com>
2019-06-28 08:36:43 +0700

    Initial Project

---
+++ src/main/java/com/example/micro_service_config/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.micro_service_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
